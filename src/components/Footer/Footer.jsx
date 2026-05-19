0c231e4c3cc9d753401f29a8cede21d473157da9
PedroFaustino <p3dro.faustino@gmail.com>
2026-05-19 15:26:28 +0100

    feat: add footer layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,16 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__copyright">
-        © 2026 MediTrack
-      </p>
+      <p className="footer__copyright">© 2026 MediTrack</p>
+
+      <nav className="footer__navigation" aria-label="Footer navigation">
+        <a className="footer__link" href="/">
+          Home
+        </a>
+        <a className="footer__link" href="https://rxnav.nlm.nih.gov/" target="_blank" rel="noreferrer">
+          RxNorm API
+        </a>
+      </nav>
     </footer>
   );
 }
