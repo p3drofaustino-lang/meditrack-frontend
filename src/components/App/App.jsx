e24b93bc74bb9fbcca992c32df89fce78e6967d0
PedroFaustino <p3dro.faustino@gmail.com>
2026-05-19 08:26:23 +0100

    feat: add main application routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import About from "../About/About";
@@ -8,8 +9,24 @@ function App() {
   return (
     <div className="page">
       <Header />
-      <Main />
-      <About />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Main />
+              <About />
+            </>
+          }
+        />
+
+        <Route
+          path="/search"
+          element={<Main />}
+        />
+      </Routes>
+
       <Footer />
     </div>
   );
