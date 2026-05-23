e2bc757832e8ca90892a17346a64abb7daebcdd0
PedroFaustino <p3dro.faustino@gmail.com>
2026-05-23 13:21:13 +0100

    style: refine medication card layout

--- src/components/MedicationCard/MedicationCard.jsx
+++ src/components/MedicationCard/MedicationCard.jsx
@@ -3,9 +3,25 @@ import "./MedicationCard.css";
 function MedicationCard({ medication }) {
   return (
     <article className="medication-card">
-      <h3 className="medication-card__title">{medication.name}</h3>
-      <p className="medication-card__type">Type: {medication.tty}</p>
-      <p className="medication-card__id">RxCUI: {medication.rxcui}</p>
+      <span className="medication-card__badge">{medication.tty}</span>
+
+      <div className="medication-card__section">
+        <p className="medication-card__label">Medication</p>
+        <h3 className="medication-card__title">{medication.name}</h3>
+      </div>
+
+      {medication.synonym && (
+        <div className="medication-card__section">
+          <p className="medication-card__label">Formulation / synonym</p>
+          <p className="medication-card__text">{medication.synonym}</p>
+        </div>
+      )}
+
+      <div className="medication-card__section">
+        <p className="medication-card__label">RxCUI</p>
+        <p className="medication-card__text">{medication.rxcui}</p>
+      </div>
+
       <button className="medication-card__button" type="button">
         Save
       </button>
